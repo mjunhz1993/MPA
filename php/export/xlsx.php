18c357d239627f29677b3c3f98c6f255105a4242
mjunhz1993 <marko.mihalic@teoten.si>
2025-03-15 15:38:46 +0100

    Update xlsx.php

--- php/export/xlsx.php
+++ php/export/xlsx.php
@@ -1,10 +1,16 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT']. '/crm/php/SQL/SQL.php');
 include(loadPHP('export/extra'));
-include(loadPHP('export/excel/Classes/PHPExcel'));
-include(loadPHP('export/excel/Classes/PHPExcel/Writer/Excel2007'));
+
+require $GLOBALS['MAP']['HOME'].'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 function izvoz($SQL){
+    if(!isset($_GET['id']) || $_GET['id'] != ''){ return ['error' => 'No_ID']; }
+    $_POST['id'] = $_GET['id'];
+    
     $query = get_table_query($SQL);
     if(!$query){ return ['error' => 'No_query_found']; }
 
@@ -17,46 +23,51 @@ function izvoz($SQL){
     $A->execute();
     $result = $A->get_result();
     if ($result->num_rows == 0) { return ['error' => 'No_data']; }
-
-    $objPHPExcel = new PHPExcel();
-
-    $objPHPExcel->getProperties()->setCreator("Oktagon-it")
+    
+    $spreadsheet = new Spreadsheet();
+    $spreadsheet
+    ->getProperties()
+    ->setCreator("Oktagon-it")
     ->setLastModifiedBy("Oktagon-it");
-
-    $do = $objPHPExcel->getActiveSheet();
-    $do->getProtection()->setSheet(true);
-    $do->setTitle($_GET['id']);
-
-    $st = 0; $elsxRow = 2;
-    while ($row = $result->fetch_assoc()){
-        if($st == 0){ addTopRow($do, $row); }
-        $elsxCol = 0;
+    
+    $sheet = $spreadsheet->getActiveSheet();
+    // $sheet->getProtection()->setSheet(true);
+    $sheet->setTitle($_GET['id']);
+    
+    $st = 0;
+    $elsxRow = 2;
+    
+    while($row = $result->fetch_assoc()){
+        if($st == 0){ addTopRow($sheet, $row); }
+        $elsxCol = 1;
         foreach($row as $r=>$v){
-            $do->SetCellValueByColumnAndRow($elsxCol, $elsxRow, $v);
+            $sheet->setCellValue([$elsxCol, $elsxRow], $v);
             $elsxCol++;
         }
-        $do->getStyle($elsxRow)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
-        $do->getStyle($elsxRow)->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+        $sheet->getStyle($elsxRow)->getAlignment()->setHorizontal('left');
+        $sheet->getStyle($elsxRow)->getAlignment()->setVertical('center');
         $st++;
         $elsxRow++;
     }
+    
+    download_xlsx($spreadsheet);
+}
 
+function addTopRow($sheet, $row, $i = 1){
+    foreach($row as $r){
+        $sheet->setCellValue([$i, 1], $r);
+        $sheet->getColumnDimension($sheet->getHighestColumn())->setAutoSize(true);
+        $i++;
+    }
+    $sheet->getStyle('1')->getFont()->setBold( true );
+}
+
+function download_xlsx($spreadsheet){
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     header('Content-Disposition: attachment;filename="'.$_GET['id'].'.xlsx"');
     header('Cache-Control: max-age=0');
-
-    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-    $objWriter->save('php://output');
-    unset($objPHPExcel);
-}
-
-function addTopRow($do, $row, $i = 0){
-    foreach($row as $r=>$v){
-        $do->SetCellValueByColumnAndRow($i, 1, $r);
-        $do->getColumnDimension($do->getHighestColumn())->setAutoSize(true);
-        $i++;
-    }
-    $do->getStyle('1')->getFont()->setBold( true );
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('php://output');
 }
 
 if(isset($_SESSION['user_id'])){ izvoz($SQL); }
